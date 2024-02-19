0b132629e230ed2efc0e672ed2669990d5ba6114
Andro Shalikashvili <a.shalikashvili@sda.gov.ge>
2024-02-19 15:56:41 +0400

    changed hw_20

---
+++ hw_20/Stack.py
@@ -0,0 +1,26 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        if self.is_empty():
+            raise Exception("Stack is empty")
+        return self.stack.pop()
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+#=================================
+            
+stack_1 = Stack()
+
+stack_1.push(2)
+stack_1.push(5)
+stack_1.push(15)
+stack_1.push(27)
+arr = stack_1.pop()
+
+print(arr)
